f89ad0510ec4109dc5d6e84e615f0abecac0c723
mahmoudhamra <mahmoudhamra@gmail.com>
2021-07-17 02:06:14 +0300

    firebase setup

---
+++ src/toDoItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function ToDoItem({ item, handleChange }) {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through",
+  };
+
+  return (
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        checked={item.completed}
+        onChange={() => handleChange(item.id)}
+      />
+      <p style={item.completed ? completedStyle : null}>{item.text}</p>
+    </div>
+  );
+}
+
+export default ToDoItem;
